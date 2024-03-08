e267ba6ec94662c3ff2f8189c4f8e6e483bf2dc9
John Patrick Madrigal <jp.madrigal07@gmail.com>
2024-03-08 20:35:59 +0800

    Add link to header

--- apps/web/module/LandingPage/components/Header.tsx
+++ apps/web/module/LandingPage/components/Header.tsx
@@ -142,11 +142,10 @@ export default function Header() {
         <div className="fixed inset-0 z-10" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Company</span>
-
+            <Link href="/" className="-m-1.5 p-1.5">
+              <span className="sr-only">Company</span>=
               <h1>AGI</h1>
-            </a>
+            </Link>
             <button
               type="button"
               className="-m-2.5 rounded-md p-2.5 text-gray-700"
